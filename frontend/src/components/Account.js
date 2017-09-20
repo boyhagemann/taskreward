a1eae1bcfef3fd4a74fe17d04e735d5b1e60a0b5
Boy Hagemann <bh@web1on1.nl>
2017-09-21 00:17:49 +0200

    Added IBAN validation

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Link from './UI/Link'
 import { Field } from 'redux-form'
 import MaxBox from './UI/MaxBox'
 import Box from './UI/Box'
@@ -7,6 +7,7 @@ import Header from './UI/Header'
 import FieldWrapper from './UI/FieldWrapper'
 import TextInput from './UI/TextInput'
 import Loading from './Loading'
+import { isIban } from '../utils/validation'
 
 export default ({ loading, handleSubmit }) => (
 
@@ -50,6 +51,16 @@ export default ({ loading, handleSubmit }) => (
           wrapper={{
             width: [1, 2/5]
           }}
+          />
+        <Field
+          component={FieldWrapper}
+          field={TextInput}
+          name="bankaccount"
+          label="IBAN bank account number"
+          description={<span>
+            By registering your account, you agree to our <Link to="" target="_blank">Services Agreement</Link> and the <Link to="https://stripe.com/connect-account/legal" target="_blank">Stripe Connected Account Agreement</Link>.
+          </span>}
+          validate={isIban}
           />
       </Box>
     </MaxBox>
